0de65683b30c131987523d01b82957c0c8711866
Ramon-Carrillo <carrillojrramon@gmail.com>
2022-08-03 21:58:59 -0500

    Test and code alert for server error

---
+++ sundaes-on-demands/src/pages/entry/test/OrderEntry.test.js
@@ -0,0 +1,19 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import { rest } from 'msw'
+import { server, Server } from '../../../mocks/server'
+import OrderEntry from '../OrderEntry'
+
+const url = 'http://localhost:3030'
+
+test('handles error for scoops and toppings routes', async () => {
+  server.resetHandlers(
+    rest.get(`${url}/scoops`, (req, res, ctx) => res(ctx.status(500))),
+    rest.get(`${url}/toppings`, (req, res, ctx) => res(ctx.status(500))),
+  )
+  render(<OrderEntry />)
+
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole('alert')
+    expect(alerts).toHaveLength(2)
+  })
+})
